9ee6031d519bd0c19411905a99f7fc6cfba6e90e
nndrey <ordosmaleus@yandex.ru>
2024-12-21 01:30:40 +0300

    add argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,10 @@
 import { program } from 'commander';
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format');
 program.parse();
